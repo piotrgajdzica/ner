6d7186125233708496884f7f155f10b43cbcbfbe
piotrek <p.gajdzica@labmatic.com>
2020-05-05 10:37:11 +0200

    updated final models

--- final_models/final-nkjp-bert.sh
+++ final_models/final-nkjp-bert.sh
@@ -33,7 +33,7 @@ python -c "import torch; print(torch.cuda.is_available()); print(torch.backends.
 
 #time python train_tagger.py "taggers/${name}" ../../data_simplified/ -m -a  -u
 time python \
- train_bert.py \
+ ../train_bert.py \
 --data_dir /net/people/plgpgajdzica/scratch/ner/data/training_datasets/nkjp/bert \
 --model_type bert \
 --labels /net/people/plgpgajdzica/scratch/ner/data/embeddings/bert/slavic/labels.txt \
