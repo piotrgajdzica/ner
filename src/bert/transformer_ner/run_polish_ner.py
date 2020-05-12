5c47723e83e2061b49f2c0bad9a1e31edd187553
Piotr Gajdzica <piotr.gajdzica@gmail.com>
2020-05-12 14:03:20 +0200

    disabled saving pretrained tokenizer

--- src/bert/transformer_ner/run_polish_ner.py
+++ src/bert/transformer_ner/run_polish_ner.py
@@ -246,7 +246,7 @@ def train(args, train_dataset, model, tokenizer, labels, pad_token_label_id):
                         model.module if hasattr(model, "module") else model
                     )  # Take care of distributed/parallel training
                     model_to_save.save_pretrained(output_dir)
-                    tokenizer.save_pretrained(output_dir)
+                    # tokenizer.save_pretrained(output_dir)
 
                     torch.save(args, os.path.join(output_dir, "training_args.bin"))
                     logger.info("Saving model checkpoint to %s", output_dir)
@@ -644,7 +644,7 @@ def main():
             model.module if hasattr(model, "module") else model
         )  # Take care of distributed/parallel training
         model_to_save.save_pretrained(args.output_dir)
-        tokenizer.save_pretrained(args.output_dir)
+        # tokenizer.save_pretrained(args.output_dir)
 
         # Good practice: save your training arguments together with the trained model
         torch.save(args, os.path.join(args.output_dir, "training_args.bin"))
